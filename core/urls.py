683710a542e7958d8e1912a1abdd867590151fdc
Angelita Santos <angelitasantos@gmail.com>
2023-01-29 08:18:48 -0300

    configure staticfiles

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +10,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('painel/', include('plataform.urls')),
     path('painel/estoque/', include('stock.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
